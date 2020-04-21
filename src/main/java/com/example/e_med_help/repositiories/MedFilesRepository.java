b4efbd3f9e68e76b0944eaf68cd518ea35b6af15
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-22 00:28:00 +0300

    6th

--- src/main/java/com/example/e_med_help/repositiories/MedFilesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.e_med_help.repositiories;
-
-import com.example.e_med_help.models.MedFile;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedFilesRepository extends CrudRepository<MedFile,Integer> {
-}
