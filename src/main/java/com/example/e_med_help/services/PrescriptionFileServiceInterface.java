e3d87675c4ff573d3930856e000df2f0aef39e4a
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-29 08:02:07 +0300

    Update PrescriptionFileServiceInterface.java

--- src/main/java/com/example/e_med_help/services/PrescriptionFileServiceInterface.java
+++ src/main/java/com/example/e_med_help/services/PrescriptionFileServiceInterface.java
@@ -4,4 +4,7 @@ import com.example.e_med_help.models.PrescriptionFile;
 
 public interface PrescriptionFileServiceInterface {
     void insertPrescriptionFile(PrescriptionFile prescriptionFile);
+    PrescriptionFile findPrescriptionFileByPatient(int id);
+    PrescriptionFile findPrescriptionFileByDoctor(int id);
+    PrescriptionFile findPrescriptionFileByFarmacy(int id);
 }
