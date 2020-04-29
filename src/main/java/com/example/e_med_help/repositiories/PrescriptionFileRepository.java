1f07c9e157dbca7915f000bdc593b62d5aecead5
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-29 08:02:14 +0300

    Update PrescriptionFileRepository.java

--- src/main/java/com/example/e_med_help/repositiories/PrescriptionFileRepository.java
+++ src/main/java/com/example/e_med_help/repositiories/PrescriptionFileRepository.java
@@ -1,7 +1,19 @@
 package com.example.e_med_help.repositiories;
 
 import com.example.e_med_help.models.PrescriptionFile;
+import com.example.e_med_help.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PrescriptionFileRepository extends CrudRepository<PrescriptionFile, Integer> {
+
+    PrescriptionFile findByFDId(User user);
+
+    PrescriptionFile findByFPId(User user);
+
+    PrescriptionFile findByFFId(User user);
+
+
+
 }
+
+
