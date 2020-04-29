8cad7c3fb4cc09bea36be1b8cf9d9c279ff85814
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-30 01:01:31 +0300

    Update PhysicianController.java

--- src/main/java/com/example/e_med_help/controllers/PhysicianController.java
+++ src/main/java/com/example/e_med_help/controllers/PhysicianController.java
@@ -1,19 +1,24 @@
 package com.example.e_med_help.controllers;
 
 import com.example.e_med_help.dtos.MockUsers;
+import com.example.e_med_help.models.MedFile;
+import com.example.e_med_help.models.PrescriptionFile;
 import com.example.e_med_help.models.User;
 import com.example.e_med_help.repositiories.UsersRepository;
+import com.example.e_med_help.services.PrescriptionFileServiceInterface;
 import com.example.e_med_help.services.RolesServiceInterface;
 import com.example.e_med_help.services.UsersServiceInterface;
+import com.example.e_med_help.utils.CreatePDF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequestMapping("/physician")
 @Controller
@@ -23,7 +28,8 @@ public class PhysicianController {
     UsersRepository usersRepository;
     @Autowired
     UsersServiceInterface userService;
-
+    @Autowired
+    PrescriptionFileServiceInterface prescriptionFileServiceInterface;
     @Autowired
     RolesServiceInterface rolesService;
 
@@ -45,7 +51,7 @@ public class PhysicianController {
         List<User> users = (List<User>) usersRepository.findAll();
         for (User u : users) {
             if (u.getURoleId().getRoleId() == 1) {
-                temp = u.getUSurname() + " " + u.getUSurname();
+                temp = u.getUName() + " " + u.getUSurname();
                 MockUsers mock = new MockUsers(u.getUId(), temp);
                 mockUsers.add(mock);
             }
@@ -53,47 +59,52 @@ public class PhysicianController {
         return mockUsers;
     }
 
+    @ModelAttribute("pharmacies")
+    public List<MockUsers> getListOfPharmacies() {
+        List<MockUsers> mocks = new ArrayList<>();
+        String temp;
+        List<User> users = (List<User>) usersRepository.findAll();
+        for (User u : users) {
+            if (u.getURoleId().getRoleId() == 3) {
+                temp = u.getUName() + " " + u.getUSurname();
+                MockUsers mock = new MockUsers(u.getUId(), temp);
+                mocks.add(mock);
+            }
+        }
+        return mocks;
+    }
+
     @PostMapping("/prescription")
-    public String getMedicalHistoty(
-            @RequestParam("doctor") String doctor,//doctor name
-            @RequestParam("patient") int patient,//patient name
+    public String fillPrescription(
+            @RequestParam("doctor") String doctor,//doctor fullname
+            @RequestParam("patient") int patient,//patient id
+            @RequestParam("pharmacy") int pharmacy,//pharmacy id
             @RequestParam("userId") int userId,//doctor id
             @RequestParam("text") String text,//prescription text
             ModelMap modelMap) {
-
         User user = userService.getUserById(patient);
-        String patint_name = user.getUSurname()+" "+user.getUName();
-
-        System.out.println(doctor);
-        System.out.println(userId);
-        System.out.println(user.getUName());
-        System.out.println(patint_name);
-        System.out.println("-----");
-        System.out.println("Text");
-        System.out.println(text);
-        System.out.println(text.equals(""));
-        System.out.println(!text.equals(""));
-
-
-        String file_name = "Prescription" + patient + ".pdf";
-        System.out.println(file_name);
-
-//     if (!text.equals("")) {
-//        MedFile mdFilePdf = new MedFile();
-//        mdFilePdf.setFUId(usersServiceImplementation.getUserById(userId));
-//        mdFilePdf.setFUName(username);
-//        mdFilePdf.setFUSurname(usersurname);
-//        mdFilePdf.setFFilename(userPdf.getOriginalFilename());
-//        try {
-//            mdFilePdf.setFData(userPdf.getBytes());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        medFilesServiceInterface.insertMedFile(mdFilePdf);
-//    } else {
-//    }
-        System.out.println("Success01");
+        String patint_name = user.getUSurname() + " " + user.getUName(); //patient fullname
+        String file_name = "Prescription" + patient + " " + pharmacy + ".pdf";
+        if (!text.equals("")) {
+            MultipartFile file = new CreatePDF().createPrescriprionPDF(doctor, patint_name, text);
+            PrescriptionFile prescription = new PrescriptionFile();
+            prescription.setfDId(userService.getUserById(userId));
+            prescription.setfPId(userService.getUserById(patient));
+            prescription.setfFId(userService.getUserById(pharmacy));
+            prescription.setfFileName(file_name);
+            try {
+                prescription.setfData(file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            prescriptionFileServiceInterface.insertPrescriptionFile(prescription);
+        }
         return "redirect:/physician/home";
+    }
+
+    @GetMapping("/medical-history")
+    public String showMed(Model model){
+        return  null;
     }
 
 }
