5413ae36577fe2265c454b82bac64bc40c514663
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-29 07:35:58 +0300

    Create PrescriptionFileRepository.java

---
+++ src/main/java/com/example/e_med_help/repositiories/PrescriptionFileRepository.java
@@ -0,0 +1,7 @@
+package com.example.e_med_help.repositiories;
+
+import com.example.e_med_help.models.PrescriptionFile;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrescriptionFileRepository extends CrudRepository<PrescriptionFile, Integer> {
+}
