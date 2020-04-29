b381aeecdc17b3c752cedf16f24912bccccce03b
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-29 08:02:10 +0300

    Update PrescriptionFileServiceImplementation.java

--- src/main/java/com/example/e_med_help/services/PrescriptionFileServiceImplementation.java
+++ src/main/java/com/example/e_med_help/services/PrescriptionFileServiceImplementation.java
@@ -1,6 +1,7 @@
 package com.example.e_med_help.services;
 
 import com.example.e_med_help.models.PrescriptionFile;
+import com.example.e_med_help.models.User;
 import com.example.e_med_help.repositiories.PrescriptionFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -8,9 +9,30 @@ public class PrescriptionFileServiceImplementation implements PrescriptionFileSe
 
     @Autowired
     PrescriptionFileRepository fileRepository;
-    
+
+    @Autowired
+    UsersServiceImplementation usersServiceImplementation;
+
     @Override
     public void insertPrescriptionFile(PrescriptionFile prescriptionFile) {
         fileRepository.save(prescriptionFile);
+    }
+
+    @Override
+    public PrescriptionFile findPrescriptionFileByPatient(int id) {
+        User user = usersServiceImplementation.getUserById(id);
+        return fileRepository.findByFPId(user);
+    }
+
+    @Override
+    public PrescriptionFile findPrescriptionFileByDoctor(int id) {
+        User user = usersServiceImplementation.getUserById(id);
+        return fileRepository.findByFDId(user);
+    }
+
+    @Override
+    public PrescriptionFile findPrescriptionFileByFarmacy(int id) {
+        User user = usersServiceImplementation.getUserById(id);
+        return fileRepository.findByFFId(user);
     }
 }
