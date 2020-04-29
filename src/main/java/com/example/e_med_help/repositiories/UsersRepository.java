b35617b52c39a43c14ad63ca7e61f6f336931a1e
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-29 04:04:11 +0300

    Update UsersRepository.java

--- src/main/java/com/example/e_med_help/repositiories/UsersRepository.java
+++ src/main/java/com/example/e_med_help/repositiories/UsersRepository.java
@@ -4,12 +4,14 @@ import com.example.e_med_help.models.Role;
 import com.example.e_med_help.models.User;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends CrudRepository<User,Integer>{
+public interface UsersRepository extends JpaRepository<User,Integer> {
     
      User findByULoginname(String uLoginname);
      
