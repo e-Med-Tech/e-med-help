70eeee70cba345194ab25f21db3e9944823d84e1
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-30 01:01:23 +0300

    Update MedFilesServiceImplementation.java

--- src/main/java/com/example/e_med_help/services/MedFilesServiceImplementation.java
+++ src/main/java/com/example/e_med_help/services/MedFilesServiceImplementation.java
@@ -1,12 +1,16 @@
 package com.example.e_med_help.services;
 
 import com.example.e_med_help.models.MedFile;
+import com.example.e_med_help.models.User;
 import com.example.e_med_help.repositiories.MedFilesRepository;
+import com.example.e_med_help.repositiories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,6 +18,9 @@ public class MedFilesServiceImplementation implements MedFilesServiceInterface {
 
     @Autowired
     MedFilesRepository medFilesRepository;
+
+    @Autowired
+    UsersServiceInterface usersServiceInterface;
 
     @Override
     public List<MedFile> getAllMedFiles() {
@@ -28,5 +35,11 @@ public class MedFilesServiceImplementation implements MedFilesServiceInterface {
     @Override
     public MedFile getMedFile(Integer id) {
         return medFilesRepository.findById(id).get();
+    }
+
+    @Override
+    public List<MedFile> getAllMedFilesById(int id) {
+        User user = usersServiceInterface.getUserById(id);
+        return (List<MedFile>) user.getMedFileCollection();
     }
 }
