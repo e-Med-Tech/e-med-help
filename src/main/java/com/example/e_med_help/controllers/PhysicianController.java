eab01a055a1bbe6e6293d0da4adf11d55b3d8676
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-29 04:04:15 +0300

    Update PhysicianController.java

--- src/main/java/com/example/e_med_help/controllers/PhysicianController.java
+++ src/main/java/com/example/e_med_help/controllers/PhysicianController.java
@@ -1,15 +1,47 @@
 package com.example.e_med_help.controllers;
 
+import com.example.e_med_help.models.User;
+import com.example.e_med_help.repositiories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/physician")
 @Controller
 public class PhysicianController {
 
+    @Autowired
+    UsersRepository usersRepository;
+
     @GetMapping("/home")
     public String goHome() {
         return "physician";
     }
+
+
+    @GetMapping("/prescription")
+    public String showPrescription(Model model) {
+        return "prescription";
+    }
+
+    @ModelAttribute("patients")
+    public List<String> getListOfRoles() {
+        List<String> patients = new ArrayList<>();
+        String temp;
+        List<User> users = usersRepository.findAll();
+        for (User u : users) {
+            temp=u.getUSurname()+" "+u.getUSurname();
+            System.out.println(temp);
+            patients.add(temp);
+        }
+        return patients;
+    }
+
+
 }
