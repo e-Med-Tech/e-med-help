190609ecb5536c503cf05131f270d00f992ff045
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-29 07:40:01 +0300

    Create PrescriptionFileServiceInterface.java

---
+++ src/main/java/com/example/e_med_help/services/PrescriptionFileServiceInterface.java
@@ -0,0 +1,7 @@
+package com.example.e_med_help.services;
+
+import com.example.e_med_help.models.PrescriptionFile;
+
+public interface PrescriptionFileServiceInterface {
+    void insertPrescriptionFile(PrescriptionFile prescriptionFile);
+}
