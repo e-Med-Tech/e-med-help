95640e2e6d9cf7fd19377a0e9a71b3f2e2db3eea
Gratsiella Kalfakakou <37407309+Gratsiella@users.noreply.github.com>
2020-04-29 17:25:32 +0300

    Update PhysicianController.java

--- src/main/java/com/example/e_med_help/controllers/PhysicianController.java
+++ src/main/java/com/example/e_med_help/controllers/PhysicianController.java
@@ -1,8 +1,11 @@
 package com.example.e_med_help.controllers;
 
 import com.example.e_med_help.dtos.MockUsers;
+import com.example.e_med_help.models.Role;
 import com.example.e_med_help.models.User;
 import com.example.e_med_help.repositiories.UsersRepository;
+import com.example.e_med_help.services.RolesServiceInterface;
+import com.example.e_med_help.utils.MockUsersMethods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +23,9 @@ public class PhysicianController {
     @Autowired
     UsersRepository usersRepository;
 
+    @Autowired
+    RolesServiceInterface rolesService;
+
     @GetMapping("/home")
     public String goHome() {
         return "physician";
@@ -34,8 +40,10 @@ public class PhysicianController {
     @ModelAttribute("patients")
     public List<MockUsers> getListOfRoles() {
         List<MockUsers> patients = new ArrayList<>();
+        Role patient = rolesService.getById(1);
+        patients = MockUsersMethods.getMockUsers(patient);
         String temp;
-        List<User> users = usersRepository.findAll();
+        List<User> users = (List<User>) usersRepository.findAll();
         for (User u : users) {
             temp=u.getUSurname()+" "+u.getUSurname();
             MockUsers mock = new MockUsers(u.getUId(),temp);
